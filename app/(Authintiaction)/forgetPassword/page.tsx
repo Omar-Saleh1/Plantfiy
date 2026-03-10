dcfc8c668dbc04f4d52656ededd3a69cc33f39a3
Omar-Saleh <os6100050@gmai.com>
2026-03-10 13:08:28 -0700

    update

--- app/(Authintiaction)/forgetPassword/page.tsx
+++ app/(Authintiaction)/forgetPassword/page.tsx
@@ -1,7 +1,103 @@
-import React from 'react'
+'use client';
 
-export default function forgetpasword() {
+import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { User, Lock, Calendar, User2, Link } from 'lucide-react';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
+
+export default function ForgetPassword() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const formSchema = z.object({
+    email: z.string().nonempty("Email required").email("Enter valid email"),
+    
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    
+    },
+  });
+
+  async function handleForgetPassword(values: z.infer<typeof formSchema>) {
+    setLoading(true);
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/v1/auth/forgot-password`, {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        toast.success(data.message );
+        console.log(data);
+        router.push("/resetCode");
+      } else {
+        toast.error(data?.message);
+      }
+    } catch {
+      toast.error("Server error. Try again later.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>page</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-sm rounded-xl shadow-lg bg-white">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold">Blantify</CardTitle>
+          <CardDescription>Log in to your Plantify account</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleForgetPassword)} className="flex flex-col gap-4">
+
+
+              {/* Email */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input {...field} placeholder="sarah@example.com" className="pl-3" />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+            
+           
+              <Button type="submit" className="w-full mt-2" disabled={loading}>
+                {loading ? "Sending..." : "Send Email"}
+              </Button>
+
+            
+            
+            </form>
+          </Form>
+        </CardContent>
+
+      
+      </Card>
+    </div>
+  );
 }
